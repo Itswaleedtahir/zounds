897696c3af38259701f3f8d265b59a6e30baab36
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2024-12-06 11:05:54 +0500

    auth completed

--- routes/admin.js
+++ routes/admin.js
@@ -4,18 +4,29 @@ const router = Router();
 // Middlewares
 const authPolicy = require("../utils/authPolicy");
 const {checkRole}=require("../utils")
+const {checkPermission}=require("../utils")
 const {ROLES} = require("../utils/constant")
 // Controllers
 const controller = require("../controllers/adminController");
 console.log("roles",ROLES)
 // Routes
-router.post("/createAdmin",authPolicy,checkRole([ROLES.ADMIN]),controller.addAdmin)
+router.post("/createAdmin",authPolicy,checkPermission("Labels,Add","Super_Admin,Add"),controller.addAdmin)
 router.post("/adminLogin",controller.adminLogin)
 router.post("/adminForgot",controller.forgetPassword)
 router.post("/adminVerify",controller.adminVerify)
 router.post("/adminReset",controller.resetPassword)
 router.post("/adminUpdate",authPolicy,controller.updateAdmin)
-router.post("/labelCreate",authPolicy,checkRole([ROLES.ADMIN]),controller.labelCreation)
+router.post("/createActions", controller.createActions);
+router.get("/getActions", controller.getActions);
+router.get("/getAllLabels",authPolicy ,controller.getAllLabels);
+router.get("/getAllUsers",authPolicy,controller.getAllUsers);
+router.get("/getAllCustomers",authPolicy,controller.getCustomers);
+router.post("/changePassword",authPolicy,controller.changePassword);
+router.get("/getAllDataOfLabel/:id",authPolicy,controller.getAllSongsOfLabel);
+router.post("/labelCreate",authPolicy,checkPermission("Labels,Add"),controller.labelCreation)
+router.put("/updateCustomer/:id",authPolicy,controller.updateCustomers);
+router.delete("/deleteCustomer/:id",authPolicy,controller.deleteCustomer);
 router.post("/adminDelete/:id",authPolicy,checkRole([ROLES.ADMIN]),controller.deleteAdmin)
+router.put("/editUser/:userId",authPolicy,controller.updateUser)
 
 module.exports = router;

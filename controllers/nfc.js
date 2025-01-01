b788eb826dd8c941ed5632bc56675334c27990c2
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2025-01-01 12:10:40 +0500

    Update nfc.js

--- controllers/nfc.js
+++ controllers/nfc.js
@@ -1,32 +1,40 @@
 const NFC = require("../models/nfc")
 const UserAlbum = require("../models/userAlbums")
+const Album = require("../models/album")
 const Admin = require("../models/dashboardUsers")
 const {generateOTP,generateToken} = require("../utils/index")
 const { parse } = require('json2csv');
 const fs = require('fs');
+const album = require("./album");
 module.exports = {
-    createNfc: async(req,res)=>{
+   createNfc: async(req, res) => {
         const { album_id, numberOfNFC, activate, label_id: bodyLabelId } = req.body;
         let label_id;
-    
+        let labelUser;
+        let labelName = ''; // To store the label name
         const userRole = req.token.role.toString().toUpperCase();
+        
         if (userRole === 'SUPER_ADMIN') {
-            // Use label_id from request body if the role is SUPER_ADMIN
             label_id = bodyLabelId;
+            labelUser = await Admin.findById(label_id);
         } else if (userRole === 'LABEL') {
-            // Directly use the user's ID if the role is LABEL
             label_id = req.token._id;
+            labelUser = await Admin.findById(label_id);
         } else if (userRole === 'LABEL_STAFF') {
-            // Find the label ID from the createdBy if the role is LABEL_STAFF
-            const labelUser = await Admin.findById(req.token.createdBy);
-            label_id = labelUser ? labelUser._id : null;  // Ensure that createdBy points to the LABEL's ID
+            labelUser = await Admin.findById(req.token.createdBy);
+            label_id = labelUser ? labelUser._id : null;
         }
     
-        if (!label_id) {
+        if (!label_id || !labelUser) {
             return res.status(400).send({ message: 'Missing label ID or unauthorized access' });
         }
+    
+        // If label user exists, concatenate their name
+        if (labelUser) {
+            labelName = `${labelUser.firstName} ${labelUser.lastName}`;
+        }
+    
         const nfcChips = [];
-
         try {
             for (let i = 0; i < numberOfNFC; i++) {
                 let code = generateOTP();  // 6-digit OTP
@@ -49,13 +57,34 @@ module.exports = {
                 nfcChips.push(newNFC);
             }
     
-            await NFC.insertMany(nfcChips); // Bulk insert generated NFC chips
-           return res.status(201).send({ message: 'NFCs created successfully', data: nfcChips.length });
+            const album = await Album.findById(album_id);
+            console.log("Album", album);
+            await NFC.insertMany(nfcChips);
+    
+            const csvData = nfcChips.map(item => ({
+                label_name: labelName, // Using fetched label name
+                album_name: album ? album.title : '',
+                token: item.token,
+                code: item.code,
+                activationDate: item.activationDate,
+                status: item.status,
+                mapped: item.mapped,
+                createdAt: item.createdAt,
+                updatedAt: item.updatedAt
+            }));
+    
+            const { parse } = require('json2csv');
+            const csv = parse(csvData);
+    
+            res.header('Content-Type', 'text/csv');
+            res.header('Content-Disposition', 'attachment; filename=NFCs.csv');
+            res.send(csv);
         } catch (error) {
-            console.log("error",error)
-           return res.status(500).send({ message: 'Error creating NFCs', error: error.message });
+            console.log("error", error);
+            return res.status(500).send({ message: 'Error creating NFCs', error: error.message });
         }
     },
+    
     getAllNfcs: async(req,res)=>{
         const userRole = req.token.role.toString().toUpperCase();
         const userId = req.token._id; // User's ID from the token

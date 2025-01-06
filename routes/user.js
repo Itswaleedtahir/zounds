30037b69a7c0f6249357429e4892d6d3760db5ea
Muhammad Mujeeb <mohammad.mujeeb.051@gmail.com>
2025-01-06 11:41:03 +0500

    bug fixed

--- routes/user.js
+++ routes/user.js
@@ -24,7 +24,9 @@ router.post("/appleLogin",controller.appleLogin)
 router.get("/getPreference",authPolicy,controller.userArtists)
 router.get("/getRedeemAlbums",authPolicy,controller.getAlbumsOfUserRedeemed)
 router.get("/getAllArtists",authPolicy,controller.getAllArtists)
+// By Mujeeb
 router.get("/getAllArtistsDownload",authPolicy,controller.getAllArtistsDownload)
+
 router.get("/getAllGenres",authPolicy,controller.getAllGenre)
 router.post("/downloadArtist",authPolicy,controller.downloadArtist)
 router.get("/getDownloadArtists",authPolicy,controller.getDownloadedArtists)

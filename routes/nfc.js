17facc8b33d44f61eb9722602fb9ef8289af7fff
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2025-01-07 12:02:09 +0500

    Update nfc.js

--- routes/nfc.js
+++ routes/nfc.js
@@ -1,14 +1,16 @@
 const { Router } = require("express");
 const router = Router();
-
+// const multer = require('multer');
 // Middlewares
 const authPolicy = require("../utils/authPolicy");
+// const upload = multer({ storage: multer.memoryStorage() });
 // Controllers
 const controller = require("../controllers/nfc");
 const {checkPermission}=require("../utils")
 // Routes
 
 router.post("/createNfc",authPolicy,checkPermission("Manage NFC,Add"),controller.createNfc)
+router.post('/importNfc',authPolicy,checkPermission("Manage NFC,Add"),controller.importNfc)
 router.get("/getAllNfc",authPolicy,checkPermission("Manage NFC,View"),controller.getAllNfcs)
 router.get("/getAllNfcOnStatus",authPolicy,checkPermission("Manage NFC,View"),controller.getNfcsOnStatus)
 router.get("/downloadCsv",authPolicy,checkPermission("Manage NFC,View"),controller.downloadCsv)

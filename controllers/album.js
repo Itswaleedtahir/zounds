eaa566fbeefeab1dc5df536e7d913fc75ba31f34
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2025-01-08 16:47:33 +0500

    Update album.js

--- controllers/album.js
+++ controllers/album.js
@@ -18,7 +18,7 @@ module.exports = {
                 label_id = labelUser ? labelUser._id : null; // Ensure that createdBy points to the LABEL's ID
             }
         }
-        const { artist_id, songs_id, title, release_date, cover_image, photos_id ,isFeatured} = req.body;
+        const { artist_id, songs_id, title, release_date, cover_image, photos_id ,isFeatured,description} = req.body;
 
         // Simplified validation checks
         if (!Array.isArray(artist_id)) {
@@ -40,6 +40,7 @@ module.exports = {
                 songs_id,
                 title,
                 isFeatured,
+                description,
                 release_date,
                 photos_id: photos_id || null,
                 cover_image: cover_image || null
